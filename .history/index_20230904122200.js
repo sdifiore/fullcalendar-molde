0cac668ab1274e36bee0eb43dda8d235dccd178a
Sergio Di Fiore <difiores@outlook.com>
2023-09-04 12:24:44 -0300

    Erro index.js

---
+++ .history/index_20230904122200.js
@@ -0,0 +1,25 @@
+import { createCalendar } from './calendar.js';
+import { handleDateClick } from './handleDateClick.js';
+import { getCalendarElements } from './getCalendarElements.js';
+import { createAndRenderCalendars } from './createAndRenderCalendars.js';
+
+document.addEventListener('DOMContentLoaded', function ()
+{
+    const calendarEls = getCalendarElements(calendarNames);
+
+    // const selectedDayElement = document.getElementById('selectedDay');
+
+    const calendars = createAndRenderCalendars(calendarEls, salaNames, FullCalendar);
+
+    for (let i = 0; i < salaNames.length; i++) {
+      calendars[i] = createCalendar(calendarEls[i], salaNames[i], FullCalendar);
+      calendars[i].render();
+    }
+
+    for (let i = 0; i < salaNames.length; i++) {
+        calendars[i].setOption('dateClick', function(info) {
+          handleDateClick(info, salaNames[i]);
+        });
+      }
+});
+
